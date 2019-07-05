f1aec2d00c5c92039bd496d30686c58bfbaa32f6
ouuan <y___o___u@126.com>
2019-07-05 18:07:58 +0800

    add opensource LICENSE information

--- main.cpp
+++ main.cpp
@@ -235,7 +235,30 @@ int main()
     
 	srand(time(0));
 	
-	puts("答题时请输出正确选项的编号，可能多选，编号间不带空格\n");
+	puts("答题时请输出正确选项的编号，可能多选，编号间不带空格，顺序随意\n");
+	puts("https://github.com/ouuan/Despised\n");
+	puts(R"(MIT License
+
+Copyright (c) 2019 ouuan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+)");
 	system("pause");
 	
 	for (int i = 0; i < PROBLEM_CNT; ++i) q.push(pdi(rand() * 1.0 / RAND_MAX, i));
